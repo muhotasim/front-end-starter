1edde56b5bd443e1ec042d2ff0ce798401460e86
Muhotasim Fuad <muhotasimF@gmail.com>
2024-03-11 21:09:12 +0600

    scss updated notification and user menu added

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -2,15 +2,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { RootState } from "../store";
 import { setTheme } from "../store/ui-store.store";
-import { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
+import { logout } from "../store/user-store.store";
 
 const TopBar:React.FC<{toggle:()=>void}> = ({ toggle })=>{
     const dispatch = useDispatch();
     const [openNotification, setOpenNotification] = useState(false);
+    const [openUserDetails, setOpenUserDetails] = useState(false);
 
     const toggleNotification = ()=>setOpenNotification(!openNotification);
+    const toggleUserDetails= ()=>setOpenUserDetails(!openUserDetails);
     const ui = useSelector((state:RootState)=>state.ui);
-    const changeTheme = (e)=>{
+    const changeTheme = (e:ChangeEvent<HTMLSelectElement>)=>{
         dispatch(setTheme({theme: e.target.value}))
     }
     return <>
@@ -26,18 +29,26 @@ const TopBar:React.FC<{toggle:()=>void}> = ({ toggle })=>{
                     </select></li>
                 <li>
                     <a onClick={toggleNotification}><span><span className='fa-icon fa fa-bell'> </span></span></a>
-                    <div className={"notification-details "+(openNotification?'open-notification': '')}>
+                    <div className={"menu-details "+(openNotification?'open-menu': '')}>
                         <ul>
                             <li><a href="">Notification 1</a></li>
                             <li><a href="">Notification 2</a></li>
                             <li><a href="">Notification 3</a></li>
-                            <li className="see-all-notification"><a href="">See All</a></li>
+                            <li className="see-all-menu"><a href="">See All</a></li>
                         </ul>
                     </div>    
                 </li>
                 
                 <li>
-                    <a><span className="pr-5"><span className='fa-icon fa fa-user'/></span> User</a>
+                    <a onClick={toggleUserDetails}><span className="pr-5"><span className='fa-icon fa fa-user'/></span> User</a>
+                    <div className={"menu-details "+(openUserDetails?'open-menu': '')}>
+                        <div className="user__details">
+                            <img className="img"/>
+                            <div className="actions">
+                                <button className="btn btn-md btn-primary">Change Password</button><button onClick={()=>{dispatch(logout())}} className="btn btn-md btn-primary">Logout</button>
+                            </div>
+                        </div>
+                    </div>  
                 </li>
             </ul>
         </nav>
