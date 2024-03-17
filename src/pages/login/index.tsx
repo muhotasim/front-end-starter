8b1474b0fdded2790f254d3250bd987198408cc2
Muhotasim Fuad <muhotasimF@gmail.com>
2024-03-17 13:20:42 +0600

    updated

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,30 +8,30 @@ type LoginInput = {
     email: string;
     password: string;
 };
-const LoginPage:React.FC = ()=>{
+const LoginPage: React.FC = () => {
     const dispatch = useDispatch();
-    const {isLoading, loggedIn} = useSelector((state:RootState)=>state.users)
-    const [loginData, setLoginData] = useState<LoginInput>({email: '', password: ''})
-    const onChangeFormData = (event: React.ChangeEvent<HTMLInputElement>)=>{
-        setLoginData({...loginData, [event.target.name]: event.target.value})
+    const { isLoading, loggedIn } = useSelector((state: RootState) => state.users)
+    const [loginData, setLoginData] = useState<LoginInput>({ email: '', password: '' })
+    const onChangeFormData = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginData({ ...loginData, [event.target.name]: event.target.value })
     }
-    if(loggedIn){
-       return <Navigate to={'/'}/>
+    if (loggedIn) {
+        return <Navigate to={'/'} />
     }
     return <div className='page login-page'>
         <div className='login__container'>
-            <h4 className='mb-15 section-title'><span className='mr-5'><span className='fa-icon fa fa-sign-in-alt'/></span> Login</h4>
+            <h4 className='mb-15 section-title'><span className='mr-5'><span className='fa-icon fa fa-sign-in-alt' /></span> Login</h4>
             <div className='login-form px-15 animate-fade-in'>
                 <div className='input-box mb-15'>
                     <label className='form-label'>Email</label>
-                    <input type="text" name='email' value={loginData.email} onChange={onChangeFormData} className='input'/>
+                    <input type="text" name='email' value={loginData.email} onChange={onChangeFormData} className='input' />
                 </div>
                 <div className='input-box mb-15'>
                     <label className='form-label'>Password</label>
-                    <input type="text" name='password' value={loginData.password} onChange={onChangeFormData} className='input'/>
+                    <input type="text" name='password' value={loginData.password} onChange={onChangeFormData} className='input' />
                 </div>
                 <div className='input-box mb-15'>
-                    <button disabled={isLoading} className='btn btn-md btn-primary btn-block' onClick={()=>{ userActions.login(loginData.email, loginData.password)(dispatch) }}>Login <span className='ml-5'><span className={`fa-icon fa ${isLoading?'fa-spin fa-sync':'fa-arrow-right'}`} color='#fff'></span></span></button>
+                    <button disabled={isLoading || !loginData.email || !loginData.password} className='btn btn-md btn-primary btn-block' onClick={() => { userActions.login(loginData.email, loginData.password)(dispatch) }}>Login <span className='ml-5'><span className={`fa-icon fa ${isLoading ? 'fa-spin fa-sync' : 'fa-arrow-right'}`} color='#fff'></span></span></button>
                     <p className='mt-5'>Forgot password click <Link to='/forgot-password' className='text-link'>here</Link></p>
                 </div>
             </div>
