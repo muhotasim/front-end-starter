344d0ffb932b35527836faccc43810de2d7ee0fb
Muhotasim Fuad <muhotasimF@gmail.com>
2024-03-17 13:36:03 +0600

    updated theme and network connection

--- src/components/theme.provider.tsx
+++ src/components/theme.provider.tsx
@@ -1,12 +1,33 @@
 
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux";
 import { RootState } from '../store';
 const ThemeProvider:React.FC<{children:React.ReactElement}> = ({ children })=>{
     const ui = useSelector((state:RootState)=>state.ui)
     const theme = ui.theme;
+    const [isOnline, setIsOnline] = useState(true);
 
-    return <div className={theme}>{children}</div>;
+    useEffect(() => {
+        const handleOnline = () => setIsOnline(true);
+        const handleOffline = () => setIsOnline(false);
+
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+
+        return () => {
+            window.removeEventListener('online', handleOnline);
+            window.removeEventListener('offline', handleOffline);
+        };
+    }, []);
+
+    return <div className={theme}>
+        {children}
+        {isOnline ? (
+            <p className="network-connection">You are connected to the internet.</p>
+        ) : (
+            <p className="network-connection">You are not connected to the internet.</p>
+        )}
+    </div>;
 }
 
 export default ThemeProvider;
