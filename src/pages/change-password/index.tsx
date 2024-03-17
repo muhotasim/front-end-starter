7f0dd76251be4e9f2286402e5870daac952f9102
Muhotasim Fuad <muhotasimF@gmail.com>
2024-03-17 21:37:53 +0600

    updated - change pass form

--- src/pages/change-password/index.tsx
+++ src/pages/change-password/index.tsx
@@ -18,11 +18,11 @@ const ChangePasswordPage: React.FC = () => {
     return <div className='page change-password-page animate-fade-in'>
         <div className="card change-password-card">
             <div className="card-title">
-                <h3>Change Password</h3>
+                <h3 className="section-title">Change Password</h3>
             </div>
             <div className="card-body">
                 {error&&<p className="error-message">{error}</p>}
-                {!changePasswordSuccess?<div>
+                {!changePasswordSuccess?<div className="px-15">
                     <div className='input-box mt-15'>
                         <label className='form-label'>Password</label>
                         <input type="text" name='password' value={formData.password} onChange={onChangeFormData} className='input' />
