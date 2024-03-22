633b879dc70fd6151bdae297359c3aa6f4b8689b
Muhotasim Fuad <muhotasimF@gmail.com>
2024-03-22 19:22:32 +0600

    validation added  - using css

--- src/pages/roles/modify.tsx
+++ src/pages/roles/modify.tsx
@@ -10,12 +10,12 @@ import { RoleApiService } from "../../services/roles-api.service";
 import { rolesActions } from "../../store/roles.store";
 import { permissionActions } from "../../store/permissions.store";
 
-const ModifyRole = ()=>{
-    const {id} = useParams();
+const ModifyRole = () => {
+    const { id } = useParams();
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const { isLoading, error, roles } = useSelector((state:RootState)=>state.roles)
-    const { permissionAll } = useSelector((state:RootState)=>state.permissions)
+    const { isLoading, error, roles } = useSelector((state: RootState) => state.roles)
+    const { permissionAll } = useSelector((state: RootState) => state.permissions)
     const [formData, setFormData] = useState<{
         name: string,
         is_active: boolean,
@@ -25,89 +25,92 @@ const ModifyRole = ()=>{
         is_active: true,
         permissions: []
     });
-    
+
     const [fromValidation, setFormValidation] = useState<any>({
-        name: false})
-    const onFormField = (key:string, value:any)=>{
-        let validationObject = {...fromValidation}
+        name: false
+    })
+    const onFormField = (key: string, value: any) => {
+        let validationObject = { ...fromValidation }
         validationObject[key] = false;
-        setFormData({...formData, [key]: value})
+        setFormData({ ...formData, [key]: value })
         setFormValidation(validationObject);
     }
-    const fetchData = async ()=>{
+    const fetchData = async () => {
         permissionActions.all()(dispatch);
-        
-        if(id){
+
+        if (id) {
             const roleService = new RoleApiService(appConst.API_URL);
-            const roleResult =await roleService.getById(id);
-            if(roleResult.type == ResponseType.success){
-                
+            const roleResult = await roleService.getById(id);
+            if (roleResult.type == ResponseType.success) {
+
                 setFormData({
                     name: roleResult.data.name,
                     is_active: roleResult.data.is_active,
-                    permissions: roleResult.data.permissions.map((d: { id: any; })=>d.id)
+                    permissions: roleResult.data.permissions.map((d: { id: any; }) => d.id)
                 })
             }
         }
     }
-    const onSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         let validationObj = {
-            name: false};
-        if(!formData.name) validationObj.name = true;
+            name: false
+        };
+        if (!formData.name) validationObj.name = true;
 
-        if(validationObj.name){
+        if (validationObj.name) {
             setFormValidation(validationObj);
             return;
         }
-        if(id){
+        if (id) {
             let result = await rolesActions.update(id, formData)(dispatch);
-            if(result && result.type == ResponseType.success){
+            if (result && result.type == ResponseType.success) {
                 navigate('/roles')
             }
-        }else{
+        } else {
             let result = await rolesActions.create(formData)(dispatch);
-            if(result && result.type == ResponseType.success){
+            if (result && result.type == ResponseType.success) {
                 navigate('/roles')
             }
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
     }, [])
     return <div className='page dashboard-page animate-fade-in'>
-    <h2 className="mt-15 mb-15"> {id?'Update':'Create'} Role</h2>
-    <form onSubmit={onSubmit}>
-        {error&&<p className="error-message">{error}</p>}
-        <div className="form-group mt-15">
-            <label className={"form-label "+(fromValidation.name?"validation":"")}>Name</label>
-            <input className={"input "+(fromValidation.name?"validation-error":"")} value={formData.name} onChange={e=>onFormField('name', e.target.value)}/>
-        </div>
-        <div className="form-group mt-15">
-            <label className="form-label pb-15">Permissions</label>
-            <div className="multi-checkbox-container">
-            {permissionAll.map((permission,index)=>(<div key={index} className="multi-checkbox-item">
-                <Checkbox key={index} label={permission.name} checked={formData.permissions.includes(permission.id)} onChange={(v)=>{ 
-                let tempRoles = [...formData.permissions]
-                let indexOfRole = tempRoles.findIndex(r=>r==permission.id);
-                if(indexOfRole!=-1){
-                    tempRoles.splice(indexOfRole, 1)
-                }else{
-                    tempRoles.push(permission.id)
-                }
-                setFormData({...formData, permissions: [...tempRoles]})
-             }}/>
-            </div>))}
+        <h2 className="mt-15 mb-15"> {id ? 'Update' : 'Create'} Role</h2>
+        <form onSubmit={onSubmit}>
+            <div className="modify-inputs-400">
+                {error && <p className="error-message">{error}</p>}
+                <div className="form-group mt-15">
+                    <label className={"form-label " + (fromValidation.name ? "validation" : "")}>Name</label>
+                    <input className={"input " + (fromValidation.name ? "validation-error" : "")} value={formData.name} onChange={e => onFormField('name', e.target.value)} />
+                </div>
             </div>
-            
-        </div>
-        <div className="form-group mt-15">
-        <Checkbox label="Is Active" checked={formData.is_active} onChange={(v)=>{ onFormField('is_active', v) }}/>
-            
-        </div>
-        <button disabled={isLoading} className="btn btn-md btn-primary float-right"> <i className={`fa ${isLoading?'fa-sync fa-spin':'fa-save'}`}></i> {id?"Update":"Save"}</button>
-    </form>
+
+            <div className="form-group mt-15">
+                <label className="form-label pb-15">Permissions</label>
+                <div className="multi-checkbox-container">
+                    {permissionAll.map((permission, index) => (<div key={index} className="multi-checkbox-item">
+                        <Checkbox key={index} label={permission.name} checked={formData.permissions.includes(permission.id)} onChange={(v) => {
+                            let tempRoles = [...formData.permissions]
+                            let indexOfRole = tempRoles.findIndex(r => r == permission.id);
+                            if (indexOfRole != -1) {
+                                tempRoles.splice(indexOfRole, 1)
+                            } else {
+                                tempRoles.push(permission.id)
+                            }
+                            setFormData({ ...formData, permissions: [...tempRoles] })
+                        }} />
+                    </div>))}
+                </div>
+            </div>
+            <div className="form-group mt-15">
+                <Checkbox label="Is Active" checked={formData.is_active} onChange={(v) => { onFormField('is_active', v) }} />
+            </div>
+            <button disabled={isLoading} className="btn btn-md btn-primary float-right"> <i className={`fa ${isLoading ? 'fa-sync fa-spin' : 'fa-save'}`}></i> {id ? "Update" : "Save"}</button>
+        </form>
     </div>
 }
 
--- src/pages/users/modify.tsx
+++ src/pages/users/modify.tsx
@@ -9,12 +9,12 @@ import { RootState } from "../../store";
 import Checkbox from "../../components/checkbox";
 import { rolesActions } from "../../store/roles.store";
 
-const ModifyUser = ()=>{
-    const {id} = useParams();
+const ModifyUser = () => {
+    const { id } = useParams();
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const { isLoading, error } = useSelector((state:RootState)=>state.user)
-    const { rolesAll } = useSelector((state:RootState)=>state.roles)
+    const { isLoading, error } = useSelector((state: RootState) => state.user)
+    const { rolesAll } = useSelector((state: RootState) => state.roles)
     const [formData, setFormData] = useState<{
         name: string
         email: string
@@ -31,101 +31,105 @@ const ModifyUser = ()=>{
     const [fromValidation, setFormValidation] = useState<any>({
         name: false,
         email: false,
-        password: false})
-    const onFormField = (key:string, value:any)=>{
-        let validationObject = {...fromValidation}
+        password: false
+    })
+    const onFormField = (key: string, value: any) => {
+        let validationObject = { ...fromValidation }
         validationObject[key] = false;
-        setFormData({...formData, [key]: value})
+        setFormData({ ...formData, [key]: value })
         setFormValidation(validationObject);
     }
-    const fetchData = async ()=>{
+    const fetchData = async () => {
         rolesActions.all()(dispatch)
-        if(id){
+        if (id) {
             const userService = new UserApiService(appConst.API_URL);
-            const userResult =await userService.getById(id);
-            if(userResult.type == ResponseType.success){
-                
+            const userResult = await userService.getById(id);
+            if (userResult.type == ResponseType.success) {
+
                 setFormData({
                     name: userResult.data.name,
                     email: userResult.data.email,
                     password: userResult.data.password,
                     is_active: userResult.data.is_active,
-                    roles: userResult.data.roles.map((d: { id: any; })=>d.id)
+                    roles: userResult.data.roles.map((d: { id: any; }) => d.id)
                 })
             }
         }
     }
-    const onSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         let validationObj = {
             name: false,
             email: false,
-            password: false};
+            password: false
+        };
 
-        if(!formData.name) validationObj.name = true;
-        if(!formData.email) validationObj.email = true;
-        if(!formData.password) validationObj.password = true;
+        if (!formData.name) validationObj.name = true;
+        if (!formData.email || !emailRegex.test(formData.email)) validationObj.email = true;
+        if (!formData.password) validationObj.password = true;
 
-        if(validationObj.name || validationObj.email || validationObj.password){
+        if (validationObj.name || validationObj.email || validationObj.password) {
             setFormValidation(validationObj);
             return;
         }
-        if(id){
+        if (id) {
             let result = await usersActions.update(id, formData)(dispatch);
-            if(result && result.type == ResponseType.success){
+            if (result && result.type == ResponseType.success) {
                 navigate('/users')
             }
-        }else{
+        } else {
             let result = await usersActions.create(formData)(dispatch);
-            if(result && result.type == ResponseType.success){
+            if (result && result.type == ResponseType.success) {
                 navigate('/users')
             }
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
     }, [])
     return <div className='page dashboard-page animate-fade-in'>
-    <h2 className="mt-15 mb-15"> {id?'Update':'Create'} User</h2>
-    <form onSubmit={onSubmit}>
-        {error&&<p className="error-message">{error}</p>}
-        <div className="form-group mt-15">
-            <label className={"form-label "+(fromValidation.name?"validation":"")}>Name</label>
-            <input className={"input "+(fromValidation.name?"validation-error":"")} value={formData.name} onChange={e=>onFormField('name', e.target.value)}/>
-        </div>
-        <div className="form-group mt-15">
-            <label className={"form-label "+(fromValidation.email?"validation":"")}>Email</label>
-            <input className={"input "+(fromValidation.email?"validation-error":"")} value={formData.email} onChange={e=>onFormField('email', e.target.value)}/>
-        </div>
-        <div className="form-group mt-15">
-            <label className={"form-label "+(fromValidation.password?"validation":"")}>Password</label>
-            <input className={"input "+(fromValidation.password?"validation-error":"")} type="password" value={formData.password} onChange={e=>onFormField('password', e.target.value)}/>
-        </div>
-        <div className="form-group mt-15">
-            <label className="form-label pb-15">Roles</label>
-            <div className="multi-checkbox-container">
-            {rolesAll.map((role,index)=>(<div className="multi-checkbox-item">
-                <Checkbox key={index} label={role.name} checked={formData.roles.includes(role.id)} onChange={(v)=>{ 
-                let tempRoles = [...formData.roles]
-                let indexOfRole = tempRoles.findIndex(r=>r==role.id);
-                if(indexOfRole!=-1){
-                    tempRoles.splice(indexOfRole, 1)
-                }else{
-                    tempRoles.push(role.id)
-                }
-                setFormData({...formData, roles: [...tempRoles]})
-             }}/>
-            </div>))}
+        <h2 className="mt-15 mb-15"> {id ? 'Update' : 'Create'} User</h2>
+        <form onSubmit={onSubmit}>
+
+            <div className="modify-inputs-400">
+                {error && <p className="error-message">{error}</p>}
+                <div className="form-group mt-15">
+                    <label className={"form-label " + (fromValidation.name ? "validation" : "")}>Name</label>
+                    <input className={"input " + (fromValidation.name ? "validation-error" : "")} value={formData.name} onChange={e => onFormField('name', e.target.value)} />
+                </div>
+                <div className="form-group mt-15">
+                    <label className={"form-label " + (fromValidation.email ? "validation" : "")}>Email</label>
+                    <input className={"input " + (fromValidation.email ? "validation-error" : "")} value={formData.email} onChange={e => onFormField('email', e.target.value)} />
+                </div>
+                <div className="form-group mt-15">
+                    <label className={"form-label " + (fromValidation.password ? "validation" : "")}>Password</label>
+                    <input className={"input " + (fromValidation.password ? "validation-error" : "")} type="password" value={formData.password} onChange={e => onFormField('password', e.target.value)} />
+                </div>
             </div>
-            
-        </div>
-        <div className="form-group mt-15">
-        <Checkbox label="Is Active" checked={formData.is_active} onChange={(v)=>{ onFormField('is_active', v) }}/>
-            
-        </div>
-        <button disabled={isLoading} className="btn btn-md btn-primary float-right"> <i className={`fa ${isLoading?'fa-sync fa-spin':'fa-save'}`}></i> {id?"Update":"Save"}</button>
-    </form>
+            <div className="form-group mt-15">
+                <label className="form-label pb-15">Roles</label>
+                <div className="multi-checkbox-container">
+                    {rolesAll.map((role, index) => (<div className="multi-checkbox-item">
+                        <Checkbox key={index} label={role.name} checked={formData.roles.includes(role.id)} onChange={(v) => {
+                            let tempRoles = [...formData.roles]
+                            let indexOfRole = tempRoles.findIndex(r => r == role.id);
+                            if (indexOfRole != -1) {
+                                tempRoles.splice(indexOfRole, 1)
+                            } else {
+                                tempRoles.push(role.id)
+                            }
+                            setFormData({ ...formData, roles: [...tempRoles] })
+                        }} />
+                    </div>))}
+                </div>
+            </div>
+            <div className="form-group mt-15">
+                <Checkbox label="Is Active" checked={formData.is_active} onChange={(v) => { onFormField('is_active', v) }} />
+            </div>
+            <button disabled={isLoading} className="btn btn-md btn-primary float-right"> <i className={`fa ${isLoading ? 'fa-sync fa-spin' : 'fa-save'}`}></i> {id ? "Update" : "Save"}</button>
+        </form>
     </div>
 }
 
