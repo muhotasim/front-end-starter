330372aef19df2c4f98410ded8edd7eda7e3bde2
Muhotasim Fuad <muhotasimF@gmail.com>
2024-03-22 11:24:02 +0600

    dashboard - data removed

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,19 +4,7 @@ import Select from "../../components/select";
 
 const DashboardPage:React.FC = ()=>{
     return <div className='page dashboard-page animate-fade-in'>
-        <DataTable  columns={[
-            {label: 'Id', key: 'id', dataIndex: 'id', searchable: false},
-            {label: 'Name', key: 'name', dataIndex: 'name'},
-            {label: 'Details', key: 'details', dataIndex: 'details'},
-            {label: 'Details2', key: 'details2', dataIndex: 'details2'},
-        ]} data={[
-          
-        ]} isLoading={true}/>
-        <Select placeHolder="Select"  allowSearch onSearch={(option, searchText)=>{
-            return option.value.toString().includes(searchText)
-        }} value={2} options={[...new Array(500)].map((val, index)=>({label: (index+1)+'', value: index+1}))} onChange={(val)=>{
-            console.log(val)
-        }}/>
+        
     </div>
 }
 export  default DashboardPage;
